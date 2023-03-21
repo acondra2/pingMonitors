eb785816a55a07c0e7b654db637125a5d65a808b
acondra2 <acondra2@gmail.com>
2023-03-21 09:44:43 -0400

    Create pingmonitor.sh
    This is a bash script that will monitor state changes of an IP address and output time,IP,state.

---
+++ pingmonitor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ip=$1
+state=0
+
+echo "time,ip,state"
+
+while true
+do
+    ping -c 1 $ip > /dev/null
+    if [ $? -eq 0 ]; then
+        if [ $state -eq 0 ]; then
+            state=1
+            echo "$(date +%T),$ip,up."
+        fi
+    else
+        if [ $state -eq 1 ]; then
+            state=0
+            echo "$(date +%T),$ip,down."
+        fi
+    fi
+    sleep 1
+done
